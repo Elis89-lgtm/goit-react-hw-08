159fc0a231316197635f9194e68ef7eefcc6435c
Elizabeth Dzhuravel <eliz5678@gmail.com>
2025-05-06 00:10:27 +0300

    initial commit

---
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+
+const setAuthHeader = (value) => {
+  axios.defaults.headers.common.Authorization = value;
+};
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = "";
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (credentials, thunkAPI) => {
+    try {
+      const res = await axios.post("/users/signup", credentials);
+      // After successful registration, add the token to the HTTP header
+      setAuthHeader(res.data.token);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
